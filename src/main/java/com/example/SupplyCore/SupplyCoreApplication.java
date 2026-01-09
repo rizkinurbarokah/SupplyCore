a9e1e22010bebf1c7976ab8b30f04263f856be8c
“riskinurbarokah” <rizki.nurbarok4@gmail.com>
2026-01-09 19:31:19 +0700

    change properties

--- src/main/java/com/example/SupplyCore/SupplyCoreApplication.java
+++ src/main/java/com/example/SupplyCore/SupplyCoreApplication.java
@@ -2,8 +2,10 @@ package com.example.SupplyCore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.SupplyCore")
 public class SupplyCoreApplication {
 
 	public static void main(String[] args) {
