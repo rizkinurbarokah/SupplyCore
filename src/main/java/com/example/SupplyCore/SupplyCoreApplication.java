97e46e54d04ea98a97beda473d8cfcfed5345024
“riskinurbarokah” <rizki.nurbarok4@gmail.com>
2026-01-09 13:51:33 +0700

    add first time data

---
+++ src/main/java/com/example/SupplyCore/SupplyCoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.SupplyCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyCoreApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World !!!");
+		SpringApplication.run(SupplyCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SupplyCore/SupplyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupplyCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
